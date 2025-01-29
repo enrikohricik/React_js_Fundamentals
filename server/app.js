3666f241147c01a3ca483c407bf3ac612f10a7d2
Enriko <58740157+enrikohricik@users.noreply.github.com>
2025-01-29 07:54:25 +0100

    app.js and vercel.json update

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-//loading the express module
 const express = require("express");
 const cors = require("cors");
 
@@ -7,18 +6,12 @@ const classroomRouter = require("./controller/classroom-controller");
 const subjectRouter = require("./controller/subject-controller");
 const gradeRouter = require("./controller/grade-controller");
 
-//initializing the new Express.js server
 const app = express();
-//defining the port on which the application should run on localhost
-const port = process.env.PORT || 8000;
 
-// Parsing
-app.use(express.json()); // support for application/json
-app.use(express.urlencoded({ extended: true })); // support for application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(cors())
-
-//a simple route definition with an HTTP GET method that only returns text
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -32,7 +25,5 @@ app.get("/*", (req, res) => {
   res.send("Unknown path!");
 });
 
-//setting the port on which the HTTP server should run
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// Export Express app
+module.exports = app;
