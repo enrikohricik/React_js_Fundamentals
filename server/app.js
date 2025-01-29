c4a3d10e85f5ae5441c3c370828566d1e430b891
Enriko <58740157+enrikohricik@users.noreply.github.com>
2025-01-29 11:44:05 +0100

    cors 3

--- server/app.js
+++ server/app.js
@@ -8,38 +8,21 @@ const gradeRouter = require("./controller/grade-controller");
 
 const app = express();
 
-// Middleware for JSON parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-// CORS configuration - Allow frontend domain
-app.use(cors({
-  origin: "https://react-js-fundamentals-client.vercel.app", // Povoliť iba frontendovú URL
-  methods: ["GET", "POST", "PUT", "DELETE"], // Povolené HTTP metódy
-  allowedHeaders: ["Content-Type", "Authorization"], // Povolené hlavičky
-  credentials: true // Ak používate cookies alebo autentifikáciu
-}));
-
-// Middleware na logovanie požiadaviek (voliteľné)
-app.use((req, res, next) => {
-  console.log(`Received request: ${req.method} ${req.url}`);
-  next();
-});
-
-// Root route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// API routes
 app.use("/student", studentRouter);
 app.use("/classroom", classroomRouter);
 app.use("/subject", subjectRouter);
 app.use("/grade", gradeRouter);
 
-// Fallback route for unknown paths
 app.get("/*", (req, res) => {
-  res.status(404).send("Unknown path!");
+  res.send("Unknown path!");
 });
 
 // Export Express app
